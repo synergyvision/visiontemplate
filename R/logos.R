f2c6b40c8fe3e56633bbc335ea3c2c2205ef1b90
Danny Morales <dannymorales@MacBook-Pro-de-Danny.local>
2019-04-25 15:39:15 -0400

    images path

---
+++ R/logos.R
@@ -0,0 +1,3 @@
+PORTADA<-system.file("rmarkdown", "templates", "basic", "resources","vision.png", package = "visiontemplate")
+CENTRO<-system.file("rmarkdown", "templates", "basic", "resources","visionrisk-white.png", package = "visiontemplate")
+DERECHA<-system.file("rmarkdown", "templates", "basic", "resources","visionrisk.png", package = "visiontemplate")
